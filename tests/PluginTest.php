ac069d7d85d2a4d4561a089a22908d0b39eb1744
badasswp <badasswpdev@gmail.com>
2025-06-06 11:01:35 +0100

    test: add test for pass case

--- tests/PluginTest.php
+++ tests/PluginTest.php
@@ -144,6 +144,75 @@ class PluginTest extends TestCase {
 		$this->assertConditionsMet();
 	}
 
+	public function test_create_product_fails_passes() {
+		$plugin = Mockery::mock( Plugin::class )
+			->shouldAllowMockingProtectedMethods()
+			->makePartial();
+
+		\WP_Mock::userFunction( 'absint' )
+			->andReturnUsing(
+				function ( $arg ) {
+					return intval( $arg );
+				}
+			);
+
+		\WP_Mock::userFunction( 'wp_attachment_is_image' )
+			->andReturnUsing(
+				function ( $arg ) {
+					return true;
+				}
+			);
+
+		\WP_Mock::userFunction( 'get_post' )
+			->andReturnUsing(
+				function ( $arg ) {
+					$post             = Mockery::mock( \WP_Post::class );
+					$post->post_title = 'Hello World';
+
+					return $post;
+				}
+			);
+
+		\WP_Mock::userFunction( 'is_wp_error' )
+			->andReturnUsing(
+				function ( $arg ) {
+					return $arg instanceof \WP_Error;
+				}
+			);
+
+		$plugin->shouldReceive( 'get_product_args' )
+			->andReturn(
+				[
+					'post_title'   => 'Hello World',
+					'post_content' => '',
+					'post_status'  => 'publish',
+					'post_type'    => 'product',
+				]
+			);
+
+		$plugin->shouldReceive( 'get_product_meta' )
+			->andReturn( $this->get_meta_args() );
+
+		\WP_Mock::userFunction( 'wp_insert_post' )
+				->andReturn( 2 );
+
+		\WP_Mock::userFunction( 'set_post_thumbnail' )
+			->with( 2, 1 )
+			->andReturn( null );
+
+		\WP_Mock::userFunction( 'wp_set_object_terms' )
+			->with( 2, 'simple', 'product_type' )
+			->andReturn( null );
+
+		\WP_Mock::userFunction( 'update_post_meta' )
+			->times( 18 )
+			->andReturn( null );
+
+		$plugin->create_product( 1 );
+
+		$this->assertConditionsMet();
+	}
+
 	public function test_get_product_args() {
 		$plugin = Mockery::mock( Plugin::class )
 			->shouldAllowMockingProtectedMethods()
@@ -364,5 +433,28 @@ class PluginTest extends TestCase {
 			]
 		);
 		$this->assertConditionsMet();
+	}
+
+	protected function get_meta_args() {
+		return [
+			'_visibility'    => 'visible',
+			'_stock_status'  => 'instock',
+			'total_sales'    => '0',
+			'_downloadable'  => 'no',
+			'_virtual'       => 'yes',
+			'_regular_price' => '',
+			'_sale_price'    => '',
+			'_purchase_note' => '',
+			'_featured'      => 'no',
+			'_weight'        => '',
+			'_length'        => '',
+			'_width'         => '',
+			'_height'        => '',
+			'_sku'           => '',
+			'_price'         => '',
+			'_manage_stock'  => 'no',
+			'_backorders'    => 'no',
+			'_stock'         => '',
+		];
 	}
 }
