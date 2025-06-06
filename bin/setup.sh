faaddae69b0b6313ad63627af014c1193ccf7ded
badasswp <badasswpdev@gmail.com>
2025-06-06 22:51:38 +0100

    feat: add WooCommerce plugin

--- bin/setup.sh
+++ bin/setup.sh
@@ -3,4 +3,6 @@
 wp-env run cli wp theme activate twentytwentythree
 wp-env run cli wp rewrite structure /%postname%
 wp-env run cli wp option update blogname "Create Product On Image Upload"
-wp-env run cli wp option update blogdescription "A WordPress site for plugin devlopment by Sculpt."
+wp-env run cli wp option update blogdescription "Create WooCommerce products automatically by uploading images."
+
+wp-env run cli wp plugin install woocommerce --activate
