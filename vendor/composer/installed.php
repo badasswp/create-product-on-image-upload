2e46ebb7e1bf3b0ce44394d85d715045080ace58
badasswp <badasswpdev@gmail.com>
2025-06-07 09:23:18 +0000

    push to master-built

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'badasswp/create-product-on-image-upload',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '1318d1f51fa515100726579392a1d1f544f8d94b',
+        'type' => 'package',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'badasswp/create-product-on-image-upload' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '1318d1f51fa515100726579392a1d1f544f8d94b',
+            'type' => 'package',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
