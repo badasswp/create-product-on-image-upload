ac0f667556c0748481226fba6d3b9e01a158ec89
badasswp <badasswpdev@gmail.com>
2026-05-25 15:19:17 +0000

    push to master-built

--- bin/setup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-wp-env run cli wp theme activate twentytwentythree
-wp-env run cli wp rewrite structure /%postname%
-wp-env run cli wp option update blogname "Create Product On Image Upload"
-wp-env run cli wp option update blogdescription "Create WooCommerce products automatically by uploading images."
-
-wp-env run cli wp plugin install woocommerce --activate
---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'badasswp/create-product-on-image-upload',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '2c1d90f3db2a92eeab98ed75a5e854a5bfee0d01',
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'badasswp/create-product-on-image-upload' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '2c1d90f3db2a92eeab98ed75a5e854a5bfee0d01',
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
