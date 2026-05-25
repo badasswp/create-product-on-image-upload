1a13d5136dcbaa694a19121abc0aafdbdd194337
badasswp <badasswpdev@gmail.com>
2026-05-25 00:51:48 +0000

    Build from release-1.1.0

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'badasswp/create-product-on-image-upload',
+        'pretty_version' => 'dev-release-1.1.0-built',
+        'version' => 'dev-release-1.1.0-built',
+        'reference' => '6abb12d8ad9a44af7e89e2459af1cb0688ec80b7',
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'badasswp/create-product-on-image-upload' => array(
+            'pretty_version' => 'dev-release-1.1.0-built',
+            'version' => 'dev-release-1.1.0-built',
+            'reference' => '6abb12d8ad9a44af7e89e2459af1cb0688ec80b7',
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
